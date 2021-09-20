8f5c592f24a4d8bab34737de9051d0fc37bbcff1
Pierre Petitpas <petitpaspierre@live.com>
2021-09-20 13:38:46 +0200

    Add Pokemon variables to JavaScript. Update README file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,54 @@
-let favoriteFood = 'Spicy Garlic Shrimps';
-alert('Hello world');
-document.write(favoriteFood);
+//Array of pokemons (Name, Height, Type)
+let pokemonList = [
+    {
+        name: 'Nidoking',
+        height: 4,
+        type: [
+            'Poison',
+            'Ground'
+        ]
+    },
+    {
+        name:'Venusaur',
+        height: 6,
+        type: [
+            'Grass',
+            'Poison'
+        ]
+    },
+    {
+        name: 'Machamp',
+        height: 5,
+        type: 'Fighting'
+    },
+    {
+        name: 'Golem',
+        height: 4,
+        type: [
+            'Rock',
+            'Ground'
+        ]
+    },
+    {
+        name: 'Tentacruel',
+        height: 5,
+        type: [
+            'Water',
+            'Poison' 
+        ]
+    },
+    {
+        name: 'Rhyhorn',
+        height: 3,
+        type: [
+            'Rock',
+            'Ground'
+        ]
+    },
+    {
+        name: 'Sandslash',
+        height: 3,
+        type: 'Ground'
+    }
+];
+
